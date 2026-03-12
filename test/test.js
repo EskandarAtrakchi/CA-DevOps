6e489f38bae80e2b25aa14dc3c5fa672d7ca252a
EskandarAtrakchi <isgan151@gmail.com>
2026-03-12 17:43:15 +0000

    .

---
+++ test/test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Math App Unit Tests', function() {
+  it('should verify that the environment is ready', function() {
+    assert.strictEqual(1 + 1, 2);
+  });
+});
